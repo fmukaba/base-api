c564adbf345f728b11fd5f8da0c4114ad541c046
fmukaba <fxkikomina@gmail.com>
2024-07-09 19:20:41 -0400

    explicit call load_dotenv

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 from datetime import timedelta
 from uuid import uuid4
+
+load_dotenv()
 
 class Settings():
     env_name=os.getenv('ENV', 'LOCAL')
@@ -8,7 +11,7 @@ class Settings():
     secret_key: str = os.getenv("SECRET_KEY", uuid4().hex)
     fernet_key: str = os.getenv("FERNET_KEY", uuid4().hex)
     database_url: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-    redis_url: str = os.getenv("REDIS_URL", "redis://localhost:6379")
+    redis_url: str = os.getenv("REDIS_URL")
     session_cookie_name="session"
     session_expire_time: timedelta = timedelta(hours=6)
     cookie_max_age: int = 3600 * 24
